1a1e580c3dec91501f6cf952cc64300e249e0673
sgeniusd <magic@Magic-SongmatoiMac.local>
2014-02-20 17:54:06 +0800

    加入一个读写文件的通用方法

---
+++ DR/projects/DRGame/Classes/Utils/DRUtility.h
@@ -0,0 +1,28 @@
+//
+//  Utility.h
+//  DRGame
+//
+//  Created by Magic Song on 14-2-20.
+//
+//
+
+#ifndef __DRGame__Utility__
+#define __DRGame__Utility__
+
+#include <iostream>
+#include <stdio.h>
+#include "cocos2d.h"
+
+using namespace std;
+
+class DRUtility : cocos2d::CCObject
+{
+public:
+    static string getFileByName(string pFileName);
+    static bool saveFile(char *pContent, string pFileName);
+private:
+    
+    
+};
+
+#endif /* defined(__DRGame__Utility__) */
