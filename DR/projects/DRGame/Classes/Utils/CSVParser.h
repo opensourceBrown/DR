0dd25283a288c0b3b51a41da9474b81d80021f72
sgeniusd <magic@magictekiMac-mini.local>
2014-02-20 23:45:54 -0800

    添加CSVParser

---
+++ DR/projects/DRGame/Classes/Utils/CSVParser.h
@@ -0,0 +1,51 @@
+//
+//  CSVParser.h
+//  DRGame
+//
+//  Created by magic on 14-2-20.
+//
+//
+
+#ifndef __DRGame__CSVParser__
+#define __DRGame__CSVParser__
+
+#include <iostream>
+#include "cocos2d.h"
+#include <vector>
+using namespace std;
+USING_NS_CC;
+
+class CCSVParser
+{
+public:
+    //    CCSVParse(void);
+    ~CCSVParser(void);
+    CCSVParser(istream& fin=cin, string sep=","):
+    fieldsep(sep),
+    cols(0)
+    {
+        
+    }
+    
+    //用以存储数据
+    std::vector<std::vector<std::string> > data;
+    bool openFile(const char* fileName);
+    const char* getData(unsigned int rows, unsigned int cols);
+    int findColsData(int cols, const char* value);
+    
+    inline int getCols(){return cols;}
+    inline int getRows(){return data.size();}
+    
+private:
+    string        fieldsep;
+    int            cols;
+    
+    void StringSplit(const string& str, vector<string>& tokens, const char& delimiters);
+    void split(vector<string>& field, string line);
+    int advplain(const string& line, string& fld, int);
+    int advquoted(const string& line, string& fld, int);
+    
+    
+};
+
+#endif /* defined(__DRGame__CSVParser__) */
