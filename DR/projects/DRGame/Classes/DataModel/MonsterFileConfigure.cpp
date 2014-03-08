b74dae0d6fd543a7add7b97f9abc69a90359b8bd
sgeniusd <magic@magictekiMac-mini.local>
2014-03-08 10:17:43 -0800

    添加普通怪的类

---
+++ DR/projects/DRGame/Classes/DataModel/MonsterFileConfigure.cpp
@@ -0,0 +1,9 @@
+//
+//  MonsterFileConfigure.cpp
+//  DRGame
+//
+//  Created by magic on 14-3-8.
+//
+//
+
+#include "MonsterFileConfigure.h"
---
+++ DR/projects/DRGame/Classes/DataModel/MonsterFileConfigure.h
@@ -0,0 +1,14 @@
+//
+//  MonsterFileConfigure.h
+//  DRGame
+//
+//  Created by magic on 14-3-8.
+//
+//
+
+#ifndef __DRGame__MonsterFileConfigure__
+#define __DRGame__MonsterFileConfigure__
+
+#include <iostream>
+
+#endif /* defined(__DRGame__MonsterFileConfigure__) */
